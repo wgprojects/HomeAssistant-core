home-assistant/core

Add Avocent Direct PDU Integration (power bar)

homeassistant/components/avocent_dpdu/entity.py
@@ -1,0 +1,15 @@
+"""Entity classes for the Avocent Direct PDU integration."""
+
+from __future__ import annotations
+
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .coordinator import AvocentDpduDataUpdateCoordinator
+
+
+class OutletEntity(CoordinatorEntity[AvocentDpduDataUpdateCoordinator]):
+    """Define an Outlet entity."""
+
+
+class CurrentEntity(CoordinatorEntity[AvocentDpduDataUpdateCoordinator]):
+    """Define an current sensor entity."""

wgprojects/HomeAssistant-core

homeassistant/components/avocent_dpdu/entity.py
@@ -1,0 +1,16 @@
+"""Entity classes for the Avocent Direct PDU integration."""
+
+from __future__ import annotations
+
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .coordinator import AvocentDpduDataUpdateCoordinator
+
+
+class PowerDistributionUnitOutletEntity(CoordinatorEntity[AvocentDpduDataUpdateCoordinator]):
+    """Define an Outlet entity."""
+    _attr_has_entity_name = True
+
+
+class PowerDistributionUnitCurrentEntity(CoordinatorEntity[AvocentDpduDataUpdateCoordinator]):
+    """Define an current sensor entity."""